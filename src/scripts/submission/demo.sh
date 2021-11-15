d6196b8470bec602cda88b2548ffcaf1b3d03ff6
max <amawi@dtu.dk>
2021-11-15 13:03:07 +0100

    ewalds upgrade

--- src/scripts/submission/demo.sh
+++ src/scripts/submission/demo.sh
@@ -3,12 +3,14 @@
 #SBATCH -N 1      # Minimum of 1 node
 #SBATCH -n 8     # 8 MPI processes per node
 #SBATCH --time=0-24:00:00 # 2 days of runtime (can be set to 7 days)
-#SBATCH --gres=gpu:RTX3090:1 # Request 1 GPU (can increase for more)
-#SBATCH --nodelist=s008
-#SBATCH --mail-type=END,FAIL
+
+# #SBATCH --gres=gpu:RTX3090:$1 # Request 1 GPU (can increase for more)
 
 source ~/.bashrc
+module load CUDA/11.1
+module load cuDNN
 # module load foss
+# # #SBATCH -o junk.out
 # nvidia-smi
 # nvcc --version
 conda activate pansatz
@@ -19,6 +21,7 @@ export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
 cd /home/energy/amawi/projects/nn_ansatz/src
+echo "${@:2}"
 
-echo "$@"
+
 python run_with_args.py "$@"
---
+++ src/scripts/submission/submit.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+submission_path=/home/energy/amawi/projects/nn_ansatz/src/scripts/submission/demo.sh
+
+if [[ $2 -eq 0 ]]; then
+    ngpu=1
+else
+    ngpu=$2
+fi
+
+if [ "$1" == "twoloop" ]  # the space is needed because [ is a test ]
+then
+    echo Calling twoloop submission
+
+    myArray=(bowl sin cos bowl+cos bowl+sin sin+cos bowl+sin+cos bowl+2sin+2cos)
+    myArray2=(tanh sin cos)
+    # myArray=(16 32 64 128)
+    # myArray2=(4 8 16 32)
+    for hypam in "${myArray[@]}" 
+    do 
+        for hypam2 in "${myArray2[@]}"
+        do
+            cmd="-s HEG \
+                -sim 1 1 1 \
+                -nw 1024 \
+                -n_sh 64 \
+                -n_ph 16 \
+                -nl 3 \
+                -n_det 1 \
+                -orb real_plane_waves \
+                -n_el 19 \
+                -inact $hypam \
+                -act $hypam2 \
+                -dp 1 \
+                -name 1311/19el_inact_act_sweep_1024 \
+                -n_it 10000 \
+                -lr 0.001"
+            sbatch --gres=gpu:RTX3090:$ngpu $submission_path $cmd
+            echo $hypam $hypam2
+            sleep 15
+        done
+    done
+fi
+if [ "$1" == "oneloop" ]
+then 
+    echo Calling single loop
+    myArray=(0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 6 6.5 7 7.5 8)
+    for hypam in "${myArray[@]}" 
+    do
+        #n_sh=$(( $hypam*32 ))
+        #n_ph=$(( $hypam*8 ))
+        cmd="-s HEG \
+            -sim 1 1 1 \
+            -nw 1024 \
+            -n_sh 64 \
+            -n_ph 16 \
+            -nl 3 \
+            -n_det 1 \
+            -orb real_plane_waves \
+            -n_el 19 \
+            -inact cos \
+            -act cos \
+            -dp $hypam \
+            -name 1511/el19_dp_sweep \
+            -n_it 25000  \
+            -lr 0.001"
+        sbatch --gres=gpu:RTX3090:$ngpu $submission_path $cmd
+        echo $cmd
+        sleep 15
+    done
+fi
+
+if [ "$1" == "single" ]
+then 
+    echo Calling single submission
+
+    cmd="-s HEG \
+        -sim 1 1 1 \
+        -nw 1024 \
+        -n_sh 128 \
+        -n_ph 32 \
+        -nl 3 \
+        -n_det 1 \
+        -orb real_plane_waves \
+        -n_el 19 \
+        -inact cos \
+        -act cos \
+        -dp 1 \
+        -name 1511/junk \
+        -n_it 100000 \
+        --sweep"
+    sbatch --gres=gpu:RTX3090:$ngpu $submission_path $cmd
+
+fi
+
+
+
+
